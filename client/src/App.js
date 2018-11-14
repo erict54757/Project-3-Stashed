94d4f67d5c1f204d7db82ff259206ebbfab296a2
eric taft <erict54757@gmail.com>
2018-11-14 13:44:48 -0500

    apptcust update

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import "./App.css";
 // import ManagerPortal from "./components/managerPortal";
 // import EmployeeSchedule from "./components/employeeSchedule";
-import NewAppointment from "./components/newAppointment";
+// import NewAppointment from "./components/newAppointment";
 // import SignUpModal from "./components/SignUpModal";
 // import SignInModal from "./components/SignInModal";
 // import NavBar from "./components/NavBar";
-// import ScheduledAppointment from "./components/scheduledAppointment"
+import ApptCust from "./components/ApptCust"
 // import NewAppointment from "./components/newAppointment"
 
 // class Nav extends Component {
@@ -24,8 +24,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <ScheduledAppointment/> */}
-        <NewAppointment/>
+        <ApptCust/>
+        {/* <NewAppointment/> */}
         {/* <ManagerPortal /> */}
       
         {/* <EmployeeSchedule /> */}
