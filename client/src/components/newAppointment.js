99eec3566a335a015c1f1d03135e98bbfd8aa0fb
eric taft <erict54757@gmail.com>
2018-11-20 12:03:49 -0500

    new appointment

--- client/src/components/newAppointment.js
+++ client/src/components/newAppointment.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Input, Button, Icon } from "react-materialize";
+import { Row, Input, Button, Icon, Modal } from "react-materialize";
 import API from "../utils/API";
 import "jquery";
 import "materialize-css/dist/js/materialize.js";
@@ -40,81 +40,93 @@ class NewAppointment extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <form>
-            <Row>
-              <Input
-                s={12}
-                className="black-text"
-                type="text"
-                name="employeeId"
-                placeholder="Pick a Barber"
-                value={this.state.employeeId}
-                onChange={this.handleInputChange}
-              >
-                <Icon>account_circle</Icon>
-              </Input>
 
-              <Input
-                s={6}
-                className="black-text"
-                type="date"
-                name="date"
-                placeholder="Date"
-                value={this.state.date}
-                onChange={this.handleInputChange}
-              >
-                <Icon>date_range</Icon>
-              </Input>
+      <Modal
+        actions={<Button
+          className="btn blue lighten-1 "
+          onClick={this.handleSubmit}
+        >
+          Save Appointment
+      </Button>
+        }
+        id=""
+        role="dialog"
+        header="Make A New Appointment"
+        trigger={<Button className="blue"
+          onClick={this.handleUpdateCustomer}
+        >Make An Appointment</Button>}
+      >
+        <div className="container">
+          <div className="row">
+            <form>
+              <Row>
+                <Input
+                  s={12}
+                  className="black-text"
+                  type="text"
+                  name="employeeId"
+                  placeholder="Pick a Barber"
+                  value={this.state.employeeId}
+                  onChange={this.handleInputChange}
+                >
+                  <Icon>account_circle</Icon>
+                </Input>
+              </Row>
+              <Row>
+                <Input
+                  s={12} l={6}
+                  className="black-text"
+                  type="date"
+                  name="date"
+                  placeholder="Date"
+                  value={this.state.date}
+                  onChange={this.handleInputChange}
+                >
+                  <Icon>date_range</Icon>
+                </Input>
 
-              <Input
-            name="time"
-            s={12} l={6}
-            type="select"
-            onChange={this.handleInputChange}
-            className="modalDrop"
-          >
+                <Input
+                  name="time"
+                  s={12} l={6}
+                  type="select"
+                  onChange={this.handleInputChange}
+                  className="modalDrop "
+                >
 
-            <option value="8AM">"8:00AM</option>
+                  <option value="8:00AM">8:00AM</option>
 
-            <option value="9AM">9:00AM</option>
+                  <option value="9:00AM">9:00AM</option>
 
-            <option value="10AM">10:00AM</option>
+                  <option value="10:00AM">10:00AM</option>
 
-            <option value="11AM">11:00AM</option>
+                  <option value="11:00AM">11:00AM</option>
 
-            <option value="12PM">12:00PM</option>
+                  <option value="12:00PM">12:00PM</option>
 
-            <option value="1PM">1:00PM</option>
+                  <option value="1:00PM">1:00PM</option>
 
-            <option value="2PM">2:00PM</option>
+                  <option value="2:00PM">2:00PM</option>
 
-            <option value="3PM">3:00PM</option>
+                  <option value="3:00PM">3:00PM</option>
 
-            <option value="4PM">4:00PM</option>
+                  <option value="4:00PM">4:00PM</option>
 
-            <option value="5PM">5:00PM</option>
+                  <option value="5:00PM">5:00PM</option>
 
-            <option value="6PM">6:00PM</option>
+                  <option value="6:00PM">6:00PM</option>
 
 
+<Icon>access_time</Icon>
+                </Input>
 
-          </Input>
-                <Icon>access_time</Icon>
-              
 
-              <Button
-                className="btn blue lighten-1 "
-                style={{ marginLeft: "45%", marginRight: "45%" }}
-                onClick={this.handleSubmit}
-              >
-                Save Appointment
-              </Button>
-            </Row>
-          </form>
+              </Row>
+
+
+            </form>
+          </div>
         </div>
-      </div>
+      </Modal>
     );
   }
 }
