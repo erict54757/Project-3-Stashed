075f4300dcbea235f8034d125ee600b419755535
Drew Ross <42723693+AndrewGallowitch@users.noreply.github.com>
2018-11-26 16:14:37 -0500

    deploy

--- client/src/pages/Employee.js
+++ client/src/pages/Employee.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 // import { Link, Route } from "react-router-dom";
 import "jquery";
 import "materialize-css/dist/js/materialize.js";
-<<<<<<< HEAD
-import "materialize-css/dist/css/materialize.css";
-=======
->>>>>>> e10a4ab974c4aca1db5b406cd57543f4d69ef7b9
 
 import NavBar from "../components/NavBar";
 import PortalContainer from "../components/PortalContainer";
