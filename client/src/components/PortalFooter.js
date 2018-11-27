4b602f036760b3d6c840982ddbe1e2972faf42e7
Nicole M Barry <42356133+nmbarry15@users.noreply.github.com>
2018-11-27 01:17:03 -0500

    Update PortalFooter.js

--- client/src/components/PortalFooter.js
+++ client/src/components/PortalFooter.js
@@ -8,36 +8,6 @@ import "./PortalFooter.css";
 import logo from "../Images/logo.jpg";
 
 class PortalFooter extends Component {
-// <<<<<<< HEAD
-
-//   render(){
-//       return(
-  
-  
-//   <div   className='black footer page-footer'
-//   >
-  
-//   <div className="container">
-//   <div className="row">
-//     <div className="col l6 s12 center">
-//       <h5 className="white-text">Specializing In Classic Cuts</h5>
-//       <img
-//       style={{ height: "auto", width: "110px", padding: "5px" }}
-//       src={logo}
-//       />
-//     </div>
-//     <div class="col l4 offset-l2 s12">
-//                   <h5 class="white-text">Charlotte Barber & Beard</h5>
-//                   <ul>
-//                     <li className="white-text">1200 The Plaza Suite B</li>
-//                     <li className="white-text">Charlotte, NC 28205</li>
-//                     <li className="white-text"
-//                     type="telephone">(704) 595-7795</li>
-                
-//                   </ul>
-//                 </div>
-//               </div>
-// =======
   render() {
     return (
       <div className="black footer page-footer">
