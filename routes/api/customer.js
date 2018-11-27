2f6fabeea5e3b2eb309105b241afff4fd0d1a5cd
eric taft <erict54757@gmail.com>
2018-11-26 22:01:01 -0500

    custcontact

--- routes/api/customer.js
+++ routes/api/customer.js
@@ -7,5 +7,7 @@ router
   .get(customerController.findAll);
 
 router.route("/:id").put(customerController.update)
-
+// router
+// .route("/api/sendEmail")
+// .post(customerController.sendEmail)
 module.exports = router;
