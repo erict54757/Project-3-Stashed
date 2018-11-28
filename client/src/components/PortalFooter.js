4bf2a3945a68de22e70b468c3b76b4f514e7183c
eric taft <erict54757@gmail.com>
2018-11-28 16:48:08 -0500

    m

--- client/src/components/PortalFooter.js
+++ client/src/components/PortalFooter.js
@@ -9,23 +9,34 @@ import logo from "../Images/logo.jpg";
 class PortalFooter extends Component {
   render() {
     return (
-      <div className="black footer page-footer">
-        <div className="container">
-          <div className="row">
-            <div className="col l6 s12 center">
-              <h5 style={{ marginTop: "20px" }} className="white-text">
-                Charlotte Barber & Beard
-              </h5>
-            </div>
-            <div className="col l6 s12 center">
-              <img
-                style={{ height: "auto", width: "110px", padding: "5px" }}
-                src={logo}
-              />
+   <div   className='black footer'
+>
+
+<div className="container">
+<div className="row">
+  <div className="col l6 s12 center">
+    <h5 className="white-text">Specializing In Classic Cuts</h5>
+    <img
+    style={{ height: "auto", width: "110px", padding: "5px" }}
+    src={logo}
+    />
+  </div>
+  <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Charlotte Barber & Beard </h5>
+                <ul>
+                  <li className="white-text">Address: 1200 The Plaza Suite B</li>
+                  <li className="white-text">Charlotte, NC 28205</li>
+                  <li className="white-text"
+                  type="telephone">(704) 595-7795</li>
+              
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        
+  
+</div>
+    )
     );
   }
 }
